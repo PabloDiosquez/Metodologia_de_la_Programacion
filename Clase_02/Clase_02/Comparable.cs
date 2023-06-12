dab13eeea2f65346b41eb959df7d31a2732fd843
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-06-12 19:36:16 -0300

    Actualización Clase 2

---
+++ Clase_02/Clase_02/Comparable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_02
+{
+    // Interfaz documentada en la clase anterior ... 
+    public interface Comparable
+    {
+        bool sosIgual(Comparable comparable);
+
+        bool sosMenor(Comparable comparable);
+
+        bool sosMayor(Comparable comparable);
+    }
+}
---
+++ Clase_02/Clase_02/Persona.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_02
+{
+    // Clase documentada en la clase anterior.
+    public class Persona : Comparable
+    {
+        // Atributos
+        private int dni;
+
+        private string nombre;
+
+        // Constructor
+        public Persona(int dni, string nombre)
+        {
+            this.dni = dni;
+
+            this.nombre = nombre;
+        }
+
+        // Métodos de instancia
+        public int getDni()
+        {
+            return dni;
+        }
+
+        public string getNombre()
+        {
+            return nombre;
+        }
+
+        // Métodos de la interfaz
+        public bool sosIgual(Comparable comparable)
+        {
+            return getDni() == ((Persona)comparable).getDni();
+        }
+
+        public bool sosMayor(Comparable comparable)
+        {
+            return getDni() > ((Persona)comparable).getDni();
+        }
+
+        public bool sosMenor(Comparable comparable)
+        {
+            return getDni() < ((Persona)comparable).getDni();
+        }
+    }
+}
