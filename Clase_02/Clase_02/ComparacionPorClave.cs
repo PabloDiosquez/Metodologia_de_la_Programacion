1354a910f461968a7b9a769693693293b279fbc4
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-06-20 13:35:43 -0300

    Actualización Clase 2

---
+++ Clase_02/Clase_02/ComparacionPorClave.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_02
+{
+    public class ComparacionPorClave : EstrategiaDeComparacion
+    {
+        public bool sosIgual(Comparable comparable1, Comparable comparable2)
+        {
+            return ((ClaveValor)comparable1).getClave().sosIgual(((ClaveValor)comparable2).getClave());
+        }
+
+        public bool sosMayor(Comparable comparable1, Comparable comparable2)
+        {
+            return ((ClaveValor)comparable1).getClave().sosMayor(((ClaveValor)comparable2).getClave()); 
+        }
+
+        public bool sosMenor(Comparable comparable1, Comparable comparable2)
+        {
+            return ((ClaveValor)comparable1).getClave().sosMenor(((ClaveValor)comparable2).getClave());
+        }
+    }
+}
