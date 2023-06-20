0d56a631d1a4398c72d9d66aa397a88b6a255b7a
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-06-20 15:48:26 -0300

    Actualización Clase 2

--- Clase_02/Clase_02/Diccionario.cs
+++ Clase_02/Clase_02/Diccionario.cs
@@ -6,7 +6,84 @@ using System.Threading.Tasks;
 
 namespace Clase_02
 {
-    internal class Diccionario
+    /// <summary>
+    /// Un Diccionario es una colección que almacena elementos, donde cada elemento tiene una clave asociada.
+    /// Las claves no pueden repetirse.
+    /// </summary>
+    public class Diccionario
     {
+        // Atributos
+
+        private Conjunto paresClaveValor;
+
+        // Constructor
+
+        public Diccionario() 
+        {
+            paresClaveValor = new Conjunto();
+        }
+
+        // Métodos de instancia
+
+        /// <summary>
+        /// agrega la asociación clave-valor al diccionario si es que la clave no existe en el diccionario.Si la clave existe, 
+        /// entonces reemplaza el valor asociado.
+        /// </summary>
+        /// <param name="clave">Elemento comparable</param>
+        /// <param name="valor">Elemento comparable</param>
+        public void agregar(Comparable clave, Comparable valor)
+        {
+            if (existeClave(clave))
+            {
+                parClaveValorDadaLaClave(clave).setValor(valor);
+            }
+            else 
+            {
+                paresClaveValor.agregar(new ClaveValor(clave, valor));
+            }
+        }
+
+        /// <summary>
+        /// Indica si la clave dada existe en el diccionario que recibe el mensaje.
+        /// </summary>
+        /// <param name="clave">Elemento comparable</param>
+        /// <returns>Bool</returns>
+        private bool existeClave(Comparable claveABuscar)
+        {
+            foreach (Comparable clave in getClaves())
+            {
+                if (clave.sosIgual(claveABuscar)) 
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private ClaveValor parClaveValorDadaLaClave(Comparable claveDada)
+        {
+            foreach (ClaveValor parClaveValor in paresClaveValor.getElementos())
+            {
+                if (parClaveValor.getClave().sosIgual(claveDada))
+                {
+                    return parClaveValor;
+                }
+            }
+
+            return null;
+        }
+
+        private List<Comparable> getClaves()
+        {
+            List<Comparable> claves = new List<Comparable>();
+
+            foreach (ClaveValor claveValor in paresClaveValor.getElementos()) 
+            {
+                claves.Add(claveValor.getClave());
+            }
+
+            return claves;
+        }
     }
 }
