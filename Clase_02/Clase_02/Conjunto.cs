41e5d28ef6430b16b40c20c1d9bdbe2eb5c68a5c
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-06-20 11:09:37 -0300

    Actualización Clase 2

--- Clase_02/Clase_02/Conjunto.cs
+++ Clase_02/Clase_02/Conjunto.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace Clase_02
 {
-    internal class Conjunto
+    /// <summary>
+    /// Modela un conjunto. Un conjunto es una colección que almacena elementos sin repetición.Es decir, si se intenta
+    /// almacenar un elemento que ya está en el conjunto, éste elemento no se almacena ya que sino estaría repetido.
+    /// </summary>
+    public class Conjunto
     {
+        // Atributos 
+
+        List<Comparable> elementos;
+
+        // Constructor 
+        public Conjunto()
+        {
+            elementos = new List<Comparable>();
+        }
+
+        // Métodos de instancia 
+
+        public List<Comparable> getElementos()
+        {
+            return elementos;
+        }
+
+        public void agregar(Comparable elemento)
+        {
+            if (!pertenece(elemento)) 
+            {
+                elementos.Add(elemento);
+            }
+        }
+
+        public bool pertenece(Comparable comparable) 
+        {
+            foreach (Comparable elemento in getElementos())
+            {
+                if (elemento.sosIgual(comparable))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
     }
 }
