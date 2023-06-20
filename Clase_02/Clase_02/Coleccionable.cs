a33fda7d974472da3ac262a4063e304e148758bd
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-06-20 11:45:05 -0300

    Actualización Clase 2

---
+++ Clase_02/Clase_02/Coleccionable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_02
+{
+    /// <summary>
+    /// Representa objetos que almacenan comparables.
+    /// </summary>
+    public interface Coleccionable
+    {
+        int cuantos();
+
+        Comparable minimo();
+
+        Comparable maximo();
+
+        bool contiene(Comparable comparable);
+
+        void agregar(Comparable comparable);
+    }
+}
--- Clase_02/Clase_02/Conjunto.cs
+++ Clase_02/Clase_02/Conjunto.cs
@@ -10,11 +10,11 @@ namespace Clase_02
     /// Modela un conjunto. Un conjunto es una colección que almacena elementos sin repetición.Es decir, si se intenta
     /// almacenar un elemento que ya está en el conjunto, éste elemento no se almacena ya que sino estaría repetido.
     /// </summary>
-    public class Conjunto
+    public class Conjunto : Coleccionable
     {
         // Atributos 
 
-        List<Comparable> elementos;
+        private List<Comparable> elementos;
 
         // Constructor 
         public Conjunto()
@@ -24,11 +24,19 @@ namespace Clase_02
 
         // Métodos de instancia 
 
-        public List<Comparable> getElementos()
+        /// <summary>
+        /// Describe los elementos de tipo Comparable que contiene el conjunto.
+        /// </summary>
+        /// <returns>Lista de comparables</returns>
+        private List<Comparable> getElementos()
         {
             return elementos;
         }
 
+        /// <summary>
+        /// Agrega al conjunto que recibe el mensaje el elemento dado, si éste no está repetido.
+        /// </summary>
+        /// <param name="elemento">Elemento comparable</param>
         public void agregar(Comparable elemento)
         {
             if (!pertenece(elemento)) 
@@ -37,6 +45,11 @@ namespace Clase_02
             }
         }
 
+        /// <summary>
+        /// Indica si el elemento dado está contenido en el conjunto que recibe el mensaje.
+        /// </summary>
+        /// <param name="comparable">Elemento comparable</param>
+        /// <returns>Bool</returns>
         public bool pertenece(Comparable comparable) 
         {
             foreach (Comparable elemento in getElementos())
@@ -50,5 +63,63 @@ namespace Clase_02
             return false;
         }
 
+        // Métodos de la interface
+
+        /// <summary>
+        /// Describe la cantidad de elementos del conjunto que recibe el mensaje.
+        /// </summary>
+        /// <returns>int</returns>
+        public int cuantos()
+        {
+            return elementos.Count;
+        }
+
+        /// <summary>
+        /// Describe el elemento más chico de todos los comparables del conjunto que recibe el mensaje.
+        /// </summary>
+        /// <returns>Elemento comparable</returns>
+        public Comparable minimo()
+        {
+            Comparable minimoAlMomento = elementos[0];
+
+            foreach (Comparable elemento in getElementos())
+            {
+                if (elemento.sosMenor(minimoAlMomento))
+                {
+                    minimoAlMomento = elemento;
+                }
+            }
+
+            return minimoAlMomento;
+        }
+
+        /// <summary>
+        /// Describe el elemento más grande de todos los comparables del conjunto que recibe el mensaje.
+        /// </summary>
+        /// <returns>Comparable</returns>
+        public Comparable maximo()
+        {
+            Comparable maximoAlMomento = elementos[0];
+
+            foreach (Comparable elemento in getElementos())
+            {
+                if (elemento.sosMayor(maximoAlMomento))
+                {
+                    maximoAlMomento = elemento;
+                }
+            }
+
+            return maximoAlMomento;
+        }
+
+        /// <summary>
+        /// Indica si el elemento comparable dado está contenido en el conjunto que recibe el mensaje.
+        /// </summary>
+        /// <param name="comparable">Elemento comparable</param>
+        /// <returns>Bool</returns>
+        public bool contiene(Comparable comparable)
+        {
+            return pertenece(comparable);
+        }
     }
 }
