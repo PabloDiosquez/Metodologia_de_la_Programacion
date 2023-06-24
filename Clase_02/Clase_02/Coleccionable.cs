4d332c8002de388b373c47880e9c4ea1025d593a
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-06-24 11:40:06 -0300

    Actualización Clase 2

--- Clase_02/Clase_02/Coleccionable.cs
+++ Clase_02/Clase_02/Coleccionable.cs
@@ -11,14 +11,39 @@ namespace Clase_02
     /// </summary>
     public interface Coleccionable : Iterable
     {
+        /// <summary>
+        /// Describe la cantidad de elementos que contiene el coleccionable.
+        /// </summary>
+        /// <returns>Int</returns>
         int cuantos();
 
+        /// <summary>
+        /// Describe el elemento más chico del coleccionable que recibe el mensaje.
+        /// Precondiciones:
+        /// ◽ El coleccionable no debe ser vacío.
+        /// </summary>
+        /// <returns>Elemento comparable</returns>
         Comparable minimo();
 
+        /// <summary>
+        /// Describe el elemento más grande del coleccionable que recibe el mensaje.
+        /// Precondiciones:
+        /// ◽ El coleccionable no debe ser vacío.
+        /// </summary>
+        /// <returns>Elemento comparable</returns>
         Comparable maximo();
 
+        /// <summary>
+        /// Indica si el coleccionable contiene al elemento comparable dado.
+        /// </summary>
+        /// <param name="comparable">Elemento comparable</param>
+        /// <returns>Booleano</returns>
         bool contiene(Comparable comparable);
 
+        /// <summary>
+        /// Agrega al coleccionable el elemento comparable dado.
+        /// </summary>
+        /// <param name="comparable">Elemento comparable</param>
         void agregar(Comparable comparable);
     }
 }
