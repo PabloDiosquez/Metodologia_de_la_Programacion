93de2cd03e3f7d877ed1af92e7b994647e1de866
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-07-02 10:55:48 -0300

    Actualización Clase 4

---
+++ Clase_04/Clase_04/AlumnoAdapter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MDPI;
+
+namespace Clase_04
+{
+    public class AlumnoAdapter : Student
+    {
+
+    }
+}
---
+++ Clase_04/MDPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MDPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ...
+        }
+    }
+}
