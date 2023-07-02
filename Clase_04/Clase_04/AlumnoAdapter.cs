79f26dd897963b94deacae42e5d1ff90083ae96e
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-07-02 11:19:16 -0300

    Actualización Clase 4

--- Clase_04/Clase_04/AlumnoAdapter.cs
+++ Clase_04/Clase_04/AlumnoAdapter.cs
@@ -9,6 +9,46 @@ namespace Clase_04
 {
     public class AlumnoAdapter : Student
     {
+        private Alumno alumno;
 
+        public AlumnoAdapter(Alumno alumno)
+        {
+            this.alumno = alumno;
+        }
+
+        public bool equals(Student student)
+        {
+            return alumno.sosIgual((Alumno)student);
+        }
+
+        public string getName()
+        {
+            return alumno.getNombre();
+        }
+
+        public bool greaterThan(Student student)
+        {
+            return alumno.sosMayor((Alumno)student);    
+        }
+
+        public bool lessThan(Student student)
+        {
+            return alumno.sosMenor((Alumno)student);
+
+        }
+        public void setScore(int score)
+        {
+            alumno.setCalificacion(score);
+        }
+
+        public string showResult()
+        {
+            return alumno.mostrarCalificacion();
+        }
+
+        public int yourAnswerIs(int question)
+        {
+            return alumno.responderPregunta(question);
+        }
     }
 }
--- Clase_04/MDPI/Student.cs
+++ Clase_04/MDPI/Student.cs
@@ -8,6 +8,18 @@ namespace MDPI
 {
     public interface Student
     {
+        string getName();
 
+        int yourAnswerIs(int question);
+
+        void setScore(int score);
+
+        string showResult();
+
+        bool equals(Student student);
+
+        bool lessThan(Student student);
+
+        bool greaterThan(Student student);  
     }
 }
