018a9a2c13c7cad8e3327f73869d46bf1c6da88c
Pablo Diosquez <pablodiosquez24@gmail.com>
2025-01-15 16:03:31 -0300

    RESET

--- clase1/src/main/java/coleccionable/ServicioColeccionable.java
+++
@@ -1,8 +0,0 @@
-package coleccionable;
-
-import coleccionable.Coleccionable;
-public interface ServicioColeccionable {
-    void rellenar(Coleccionable coleccionable, int cantidad);
-
-    void informar(Coleccionable coleccionable);
-}
